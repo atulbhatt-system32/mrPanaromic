2d699abacdd0a4d41eb899b001437d88725bd085
atulbhatt-system32 <atulbhatt98@gmail.com>
2021-04-20 20:02:55 +0530

    new Changes

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,18 +1,47 @@
 import React from "react";
+import {useSpring, animated} from 'react-spring'
+import { motion } from "framer-motion"
 
 import "./assets/css/body.css";
 import sideme from "./assets/images/side-me.svg";
 
 export default function Body() {
+  const props = useSpring({opacity: 1, from: {opacity: 0}})
+  const variants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+    // transition: { duration: 1 }
+  } 
   return (
     <div className="body">
       <div className="side-image-container">
         <img src={sideme} alt="not found!" />
       </div>
       <div className="me-container">
-        <div className="intro">Hello.</div>
-        <div className="intro text-color-red">I'm</div>
-        <div className="intro text-color-red">Atul.</div>
+        <motion.div 
+        style={props} 
+        transition={{ duration: 1 }}
+        className="intro">
+          Hello.
+        </motion.div>
+
+        <motion.div 
+        initial="hidden" animate="visible" 
+        variants={variants} 
+        transition={{ duration: 1.5 }}
+        className="intro text-color-red">
+          I'm
+        </motion.div>
+
+        <motion.div 
+        initial="hidden" animate="visible" 
+        transition={{ duration: 2.5 }}
+        variants={variants} 
+        className="intro text-color-red" 
+        whileHover={{ scale: 1.2, duration: 1.5 }}>
+          Atul.
+        </motion.div>
+        
       </div>
     </div>
   );
